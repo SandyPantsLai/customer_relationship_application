3fc8a69acd10836ab71fca6b31a165469ea95ef8
Sandy Lai <sandywslai@gmail.com>
2015-04-06 05:15:07 -0400

    Added add_new_contact method and Contact and Rolodex classes.

--- crm.rb
+++ crm.rb
@@ -1,10 +1,44 @@
-class CRM
+class Contact
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :notes
+
+  def initialize(id, first_name, last_name, email, notes)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @notes = notes
+  end
+end
+
+class Rolodex
   def initialize
+    @contact_list = []
+    @contact_id = 0
+  end
+
+  def add_new_contact
+  print "Enter First Name: "
+  first_name = gets.chomp
+  print "Enter Last Name: "
+  last_name = gets.chomp
+  print "Enter Email Address: "
+  email = gets.chomp.strip
+  print "Enter a Note: "
+  notes = gets.chomp
+  @contact_id += 1
+  @contact_list.push(Contact.new(@contact_id, first_name, last_name, email, notes))
+  end
+end
+
+class CRM
+  def initialize(company)
+    puts "Welcome to #{company} Customer Relations App.  Please choose a menu item."
+    @company = Rolodex.new
     main_menu
   end
 
   def call_option(user_selected)
-    add_new_contact if user_selected == 1
+    @company.add_new_contact if user_selected == 1
     modify_existing_contact if user_selected == 2
     delete_contact if user_selected == 3
     display_all_contacts if user_selected == 4
@@ -20,8 +54,8 @@ class CRM
     puts "[3] Delete a contact"
     puts "[4] Display all contact names"
     puts "[5] Display info for contact"
-    puts "[5] Display an attribute for all contacts"
-    puts "[6] Exit"
+    puts "[6] Display an attribute for all contacts"
+    puts "[7] Exit"
     puts "Enter a number: "
   end
 
@@ -32,4 +66,4 @@ class CRM
   end
 end
 
-CRM.new
+CRM.new("Bitmaker Labs")
